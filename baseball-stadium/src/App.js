4cdab183fa66693346b83eb17fb211bada72af68
SilverMaiden <h.specials@gmail.com>
2019-12-06 15:05:33 -0800

    Updating state currently not functioning correctly

---
+++ baseball-stadium/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CountRules from './components/CountRules';
+
+function App() {
+  return (
+    <div className="App">
+        <CountRules />
+    </div>
+  );
+}
+
+export default App;
